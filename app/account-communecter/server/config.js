9d2a01a900ab0851c5aa6f8b11ba0b6ef5c15c97
aboire <thomas.craipeau@gmail.com>
2019-11-04 12:08:59 +0400

    fix token

--- app/account-communecter/server/config.js
+++ app/account-communecter/server/config.js
@@ -8,8 +8,6 @@ import {
 	addUserRoles
 } from '../../authorization';
 Accounts.registerLoginHandler( 'login', function(loginRequest) {
-	//console.log('registerLoginHandler');
-	//console.log(loginRequest);
 	if (loginRequest.user && loginRequest.user.email && loginRequest.password) {
 		loginRequest.email = loginRequest.user.email;
 		loginRequest.pwd = loginRequest.password;
@@ -18,14 +16,14 @@ Accounts.registerLoginHandler( 'login', function(loginRequest) {
 		return null;
 	}
 
-	const response = HTTP.call('POST', `${ Meteor.settings.endpoint }/${ Meteor.settings.module }/person/authenticatetoken`, {
+	const response = HTTP.call('POST', `${Meteor.settings.endpoint}/${Meteor.settings.module}/person/authenticatetoken`, {
 		params: {
 			'email': loginRequest.email,
 			'pwd': loginRequest.pwd,
 			'tokenName': 'chat'
 		}
 	});
-	// console.log(response);
+
 	if (response && response.data && response.data.result === true && response.data.id) {
 
 		let userId = null;
@@ -36,25 +34,23 @@ Accounts.registerLoginHandler( 'login', function(loginRequest) {
 		} else {
 			retourId = response.data.id;
 		}
-		// console.log(response.data.account.email);
+
 
       //ok valide
 		const userM = Meteor.users.findOne({'_id':retourId});
-		// console.log(userM);
+
 		const token = response.data.token ? response.data.token : false;
 		if (userM) {
-        //Meteor.user existe
+
 			userId= userM._id;
 			if (token) {
-				//const profile = {};
-				//profile.token = token;
 				Meteor.users.update(userId, {
 					$set: {
 						token
 					}
 				});
 			}
-        //RocketChat._setRealName(userId, response.data.account.name);
+
 			Meteor.users.update(userId, {$set: {name: response.data.account.name,
 				emails: [{ address: response.data.account.email, verified: true }]}});
 
@@ -83,7 +79,13 @@ Accounts.registerLoginHandler( 'login', function(loginRequest) {
 
 
 			userId = Meteor.users.insert(newUser);
-
+			if (token) {
+				Meteor.users.update(userId, {
+					$set: {
+						token
+					}
+				});
+			}
 			if (response.data.account.email==='thomas.craipeau@gmail.com') {
 				roles.push('admin');
 			}
@@ -105,7 +107,7 @@ Accounts.registerLoginHandler( 'login', function(loginRequest) {
 		if (response.data.account.profilThumbImageUrl) {
 			setUserAvatar({ _id: userId, username: userR.username }, `${ Meteor.settings.urlimage }${ response.data.account.profilThumbImageUrl }`, '', 'url');
 		}
-		// console.log(userId);
+
 		return {
 			userId,
 			token: stampedToken.token
@@ -118,8 +120,7 @@ Accounts.registerLoginHandler( 'login', function(loginRequest) {
 });
 
 Meteor.server.method_handlers['loginco'] = function (loginRequest) {
-	//console.log('surcharge login');
-	//console.log(loginRequest);
+
 	if (loginRequest.user && loginRequest.user.email && loginRequest.password) {
 		loginRequest.email = loginRequest.user.email;
 		loginRequest.pwd = loginRequest.password;
@@ -128,14 +129,14 @@ Meteor.server.method_handlers['loginco'] = function (loginRequest) {
 		return null;
 	}
 
-	const response = HTTP.call('POST', `${ Meteor.settings.endpoint }/${ Meteor.settings.module }/person/authenticatetoken`, {
+	const response = HTTP.call('POST', `${Meteor.settings.endpoint}/${Meteor.settings.module}/person/authenticatetoken`, {
 		params: {
 			'email': loginRequest.email,
 			'pwd': loginRequest.pwd,
 			'tokenName': 'chat'
 		}
 	});
-	// console.log(response);
+
 	if (response && response.data && response.data.result === true && response.data.id) {
 
 		let userId = null;
@@ -146,27 +147,25 @@ Meteor.server.method_handlers['loginco'] = function (loginRequest) {
 		} else {
 			retourId = response.data.id;
 		}
-		// console.log(response.data.account.email);
+
 
 		//ok valide
 		const userM = Meteor.users.findOne({
 			'_id': retourId
 		});
-		// console.log(userM);
+
 		const token = response.data.token ? response.data.token : false;
 		if (userM) {
 			//Meteor.user existe
 			userId = userM._id;
 			if (token) {
-				//const profile = {};
-				//profile.token = token;
 				Meteor.users.update(userId, {
 					$set: {
 						token
 					}
 				});
 			}
-			//RocketChat._setRealName(userId, response.data.account.name);
+
 			Meteor.users.update(userId, {
 				$set: {
 					name: response.data.account.name,
@@ -205,7 +204,13 @@ Meteor.server.method_handlers['loginco'] = function (loginRequest) {
 
 
 			userId = Meteor.users.insert(newUser);
-
+			if (token) {
+				Meteor.users.update(userId, {
+					$set: {
+						token
+					}
+				});
+			}
 			if (response.data.account.email === 'thomas.craipeau@gmail.com') {
 				roles.push('admin');
 			}
@@ -248,167 +253,3 @@ Meteor.server.method_handlers['loginco'] = function (loginRequest) {
 		throw new Meteor.Error(response.data.msg);
 	}
 };
-
-let getUserQuerySelector, passwordValidator, userValidator;
-
-this.Auth || (this.Auth = {});
-
-
-/*
-  A valid user will have exactly one of the following identification fields: id, username, or email
- */
-
-userValidator = Match.Where(function(user) {
-	check(user, {
-		id: Match.Optional(String),
-		username: Match.Optional(String),
-		email: Match.Optional(String)
-	});
-	if (_.keys(user).length === !1) {
-		throw new Match.Error('User must have exactly one identifier field');
-	}
-	return true;
-});
-
-
-/*
-  A password can be either in plain text or hashed
- */
-
-passwordValidator = Match.OneOf(String, {
-	digest: String,
-	algorithm: String
-});
-
-
-/*
-  Return a MongoDB query selector for finding the given user
- */
-
-getUserQuerySelector = function(user) {
-	if (user.id) {
-		return {
-			'_id': user.id
-		};
-	} else if (user.username) {
-		return {
-			'username': user.username
-		};
-	} else if (user.email) {
-		return {
-			'emails.address': user.email
-		};
-	}
-	throw new Error('Cannot create selector from invalid user');
-};
-
-
-/*
-  Log a user in with their password
- */
-
-/*this.Auth.loginWithPassword = function(user, password) {
-
-	console.log(user);
-
-	let authToken, authenticatingUser, authenticatingUserSelector, hashedToken, passwordVerification, ref;
-	if (!user || !password) {
-		throw new Meteor.Error(401, 'Unauthorized');
-	}
-	check(user, userValidator);
-	check(password, passwordValidator);
-	authenticatingUserSelector = getUserQuerySelector(user);
-
-	const response = HTTP.call('POST', `${ Meteor.settings.endpoint }/${ Meteor.settings.module }/person/authenticate`, {
-		params: {
-			'email': user.email,
-			'pwd': password
-		}
-	});
-
-console.log(response.data);
-
-	if (!(response && response.data && response.data.result === true && response.data.id)) {
-		throw new Meteor.Error(401, 'Unauthorized');
-	}
-
-	let userId = null;
-	let retourId = null;
-
-	if (response.data.id && response.data.id.$id) {
-		retourId = response.data.id.$id;
-	} else {
-		retourId = response.data.id;
-	}
-
-
-  //ok valide
-	const userM = Meteor.users.findOne({'_id':retourId});
-  //console.log(userM);
-	if (userM) {
-    //Meteor.user existe
-		userId= userM._id;
-    //RocketChat._setRealName(userId, response.data.account.name);
-		Meteor.users.update(userId, {$set: {name: response.data.account.name,
-			emails: [{ address: response.data.account.email, verified: true }]}});
-
-	} else {
-    //Meteor.user n'existe pas
-    //username ou emails
-
-		const newUser = {
-			_id:retourId,
-			username: response.data.account.username,
-			name: response.data.account.name,
-			emails: [{ address: response.data.account.email, verified: true }],
-			createdAt: new Date(),
-			active: true,
-			type: 'user',
-			globalRoles: ['user']
-		};
-
-		let roles = [];
-
-		if (Match.test(newUser.globalRoles, [String]) && newUser.globalRoles.length > 0) {
-			roles = roles.concat(newUser.globalRoles);
-		}
-
-		delete newUser.globalRoles;
-
-
-		userId = Meteor.users.insert(newUser);
-
-		if (response.data.account.email==='thomas.craipeau@gmail.com') {
-			roles.push('admin');
-		}
-
-		// RocketChat.authz.addUserRoles(retourId, roles);
-		addUserRoles(retourId, roles);
-	}
-
-
-
-	authToken = Accounts._generateStampedLoginToken();
-
-	Meteor.users.update(userId,
-    {$push: {'services.resume.loginTokens': authToken}}
-  );
-
-  //this.setUserId(userId);
-
-
-	hashedToken = Accounts._hashLoginToken(authToken.token);
-	Accounts._insertHashedLoginToken(userId, {
-		hashedToken
-	});
-
-  console.log({
-  	authToken: authToken.token,
-  	userId
-  });
-	return {
-		authToken: authToken.token,
-		userId
-	};
-};
-*/
